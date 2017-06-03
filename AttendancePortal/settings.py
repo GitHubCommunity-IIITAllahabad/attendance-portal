cda7cf0c45dd2d57abc9cfdd2734ca93ca512cf6
ankur <hj.harshit007@gmail.com>
2017-06-03 17:11:55 +0530

    Created ldaplogin.py and serializers.py files and created function to authenticate users on ldap server

--- AttendancePortal/settings.py
+++ AttendancePortal/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from django_auth_ldap.config import LDAPSearch
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -26,7 +26,6 @@ SECRET_KEY = '7)p19%x(tj$_6@+uu!1oo(nj3tfuxj53z0i-zwn+)1zb=tqg4*'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'AttendancePortal.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -84,7 +82,6 @@ DATABASES = {
         'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -104,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -116,7 +112,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-#USE_TZ = True
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
--- attendance_portal/views.py
+++ attendance_portal/views.py
@@ -2,5 +2,4 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
-
-
+from .models import Student, StudentCourse, Course, Attendance
