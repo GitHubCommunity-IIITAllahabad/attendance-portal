07eb2ad6ffc726033e9dc71614e1d6afb00a1f53
AjiteshGupta13 <ajiteshgupta13@gmail.com>
2017-06-28 17:25:05 +0530

    Added basic template and stylesheet

--- attendance_portal/urls.py
+++ attendance_portal/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 app_name = 'attendance_portal'
 urlpatterns = [
-    url(r'^students', views.StudentView.as_view()),
-    url(r'^login', views.UserLoginView.as_view()),
-    url(r'^student/course', views.StudentCourseView.as_view()),
-    url(r'^attendance-tokens', views.AttendanceTokenView.as_view()),
-    url(r'^attendance/student', views.StudentAttendanceView.as_view()),
-    url(r'faculty/course', views.ProfessorCourseView.as_view()),
-    url(r'^attendance/course', views.ProfessorAttendanceView.as_view()),
-    url(r'^logout', views.logout)
+    url(r'^students', views.StudentView.as_view(), name='students'),
+    url(r'^login', views.UserLoginView.as_view(), name='login_user'),
+    url(r'^student/course', views.StudentCourseView.as_view(), name='students_course'),
+    url(r'^attendance-tokens', views.AttendanceTokenView.as_view(), name='tokens'),
+    url(r'^attendance/student', views.StudentAttendanceView.as_view(), name='attendance_students'),
+    url(r'faculty/course', views.ProfessorCourseView.as_view(), name='faculty_course'),
+    url(r'^attendance/course', views.ProfessorAttendanceView.as_view(), name='attendance_course'),
+    url(r'^logout', views.logout, name='logout_user'),
 ]
